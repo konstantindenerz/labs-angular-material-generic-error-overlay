8c959c3444bbd5d33fe673ea098fd7ca3078233f
Konstantin Denerz <konstantin.denerz@gmail.com>
2022-04-08 17:03:14 +0200

    update error handling

--- projects/labs-foo/src/app/error-handling/error.pipe.ts
+++ projects/labs-foo/src/app/error-handling/error.pipe.ts
@@ -5,12 +5,14 @@ export class ErrorPipe implements PipeTransform {
   transform(value: any, ...args: any[]): any {
     const messages: string[] = [];
     console.log(value);
-    if(!value){
+    if (!value) {
       return;
     }
     Object.keys(value).forEach(key => {
       if (key === 'required') {
         messages.push('The field is required')
+      } else if (key === 'minlength') {
+        messages.push(`Should contain at least ${value[key].requiredLength} characters.`)
       } else {
         messages.push(key);
       }
